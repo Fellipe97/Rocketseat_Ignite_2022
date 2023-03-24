cda66878c2c8b3b543ad86db55959a66bdf17b3b
Fellipe97 <lfalmeida97@hotmail.com>
2023-03-23 23:11:21 -0300

    update

--- reactNative/igniteteams/src/screens/Groups/index.tsx
+++ reactNative/igniteteams/src/screens/Groups/index.tsx
@@ -1,16 +1,40 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
-import { 
-    Container
+import { ListEmpty } from '@components/ListEmpty';
+
+import {
+  Container
 } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+
+
   return (
     <Container>
-      <Header/>
-      <Highlight tittle='Turmas' subtittle='jogue com a sua turma'/>
-      <GroupCard title='Galera do Ignite'/>
+      <Header />
+      <Highlight tittle='Turmas' subtittle='jogue com a sua turma' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turma?'
+          />
+        )}
+      />
+
     </Container>
   );
 }
